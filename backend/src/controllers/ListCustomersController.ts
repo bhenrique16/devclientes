7252c75829a77a0d8101a257b57ad950d05d0d4a
bhenrique16 <bh.henrique16@gmail.com>
2023-11-01 20:17:52 -0300

    Rotas da API

---
+++ backend/src/controllers/ListCustomersController.ts
@@ -0,0 +1,13 @@
+import { FastifyRequest,FastifyReply } from "fastify";
+import {ListCustomersService} from '../services/ListCustomersService'
+
+class ListCustomersController{
+    async handle(request:FastifyRequest,reply:FastifyReply){
+        const ListCustomerService = new ListCustomersService()
+
+        const customers = await ListCustomerService.execute()
+        reply.send(customers)
+    }
+}
+
+export {ListCustomersController}
---
+++ backend/src/services/CreateCustomerServices.ts
@@ -0,0 +1,26 @@
+import prismaClient from "../prisma";
+
+interface CreateCustomerProps{
+    name:string;
+    email:string;
+}
+class CreateCustomerServices{
+    async execute({name,email}:CreateCustomerProps){
+        if(!name || !email){
+            throw new Error("preencha todos os campos")
+        }
+        const customer = await prismaClient.customer.create({
+            data:{
+                name,
+                email,
+                status:true
+            }
+    
+    
+    })
+
+    return customer
+}
+}
+
+export {CreateCustomerServices}
